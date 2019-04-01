7015961ecfa706a538d9086816b7c558f6247818
ArnauCalleja <arnau.cm123@gmail.com>
2019-04-01 16:11:48 +0200

    cercle constructors

---
+++ Cercle.java
@@ -0,0 +1,39 @@
+package github;
+
+public class Cercle {
+        
+    private float x; 
+    private float y; 
+         
+    Cercle(float xx, float yy){     
+        x=xx;     
+        y=yy;       
+    }   
+         
+    Cercle(){}
+ 
+    public float getX() {
+        return x;
+    }
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+    public void setY(float y) {
+        this.y = y;
+    }
+    
+    public String toString(){
+        return "X: " + x + ", Y:" + y;
+    }
+    
+    public Boolean equals(Cercle c){
+        return c.getX()==x && c.getY()==y;
+    }
+         
+         
+
+}//Arnau Calleja Martin <arnau.cm123@gmail.com>
